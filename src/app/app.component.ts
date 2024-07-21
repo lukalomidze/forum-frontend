963ebdfaee81d4beb2e41b70422545647b30673c
lukalomidze <lomidzeluka.w@gmail.com>
2024-07-22 01:25:46 +0400

    added components to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CreatePostComponent } from "./component/create-post/create-post.component";
+import { FooterComponent } from './component/footer/footer.component';
+import { HeaderComponent } from './component/header/header.component';
+import { PostComponent } from './component/post/post.component';
+import { PostsComponent } from "./component/posts/posts.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, HeaderComponent, PostsComponent, PostComponent, FooterComponent, CreatePostComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
