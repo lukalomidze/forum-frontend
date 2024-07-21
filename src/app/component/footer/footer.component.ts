a110a0483de29169b54344aa497440bd26159a4f
lukalomidze <lomidzeluka.w@gmail.com>
2024-07-22 01:23:48 +0400

    added footer

---
+++ src/app/component/footer/footer.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [],
+  template: `
+    <footer>
+        <p>&copy; 2024 All Rights Reserved</p>
+    </footer>
+  `,
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {
+
+}
